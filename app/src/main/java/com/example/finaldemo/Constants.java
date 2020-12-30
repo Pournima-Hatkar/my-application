4b0d88146a826c2c8f818e44455925af39d1c35e
Pournima-Hatkar <57834047+Pournima-Hatkar@users.noreply.github.com>
2020-12-30 23:38:04 +0530

    Update Constants.java

--- app/src/main/java/com/example/finaldemo/Constants.java
+++ app/src/main/java/com/example/finaldemo/Constants.java
@@ -2,11 +2,11 @@ package com.example.finaldemo;
 
 public class Constants {
 
-private static final String ROOT_URL ="http://192.168.43.36/Android/v1/";
+private static final String ROOT_URL ="http://vimalhatkarhost.000webhostapp.com/Android/includes/";
 
-public static final String URL_REGISTER = ROOT_URL + "registerUser.php";
+public static final String URL_REGISTER = ROOT_URL + "insert.php";
 
-public static final String URL_LOGIN = ROOT_URL + "userLogin.php";
+public static final String URL_LOGIN = ROOT_URL + "update.php";
 
 
 
